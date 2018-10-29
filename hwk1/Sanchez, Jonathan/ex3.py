b0ccf5c88ec3cd74f41080de686b4f25e44d3f00
Jonathan Sanchez <jesanchez@wpi.edu>
2018-10-28 21:01:51 -0400

    wordlist_to_wordfreq is almost there, but a small bug detecting brackets exists

--- hwk1/Sanchez, Jonathan/ex3.py
+++ hwk1/Sanchez, Jonathan/ex3.py
@@ -10,10 +10,42 @@ def file_to_wordlist(fname):
         cleaned_line = []
         line_strs = line.split(" ")
         for word in line_strs:
-            word = word.translate(None, '.,?!":;()*')
+            word = word.translate(None, '.,?!":;()*[]')
             word = word.lower()
             if word != "":  # check for empty string here
                 cleaned_line.append(word)
         wordlist.extend(cleaned_line)
     f.close()
     return wordlist
+
+
+def wordlist_to_wordfreq(wordlist):
+    wordfreq = {}
+
+    for word in wordlist:
+        if word in wordfreq:
+            wordfreq[word] += 1
+        else:
+            wordfreq[word] = 1
+
+    return wordfreq
+
+import heapq
+
+
+def wordfreq_to_wordpriority(wordfreq):
+    wordpriority = []
+
+    # for realzies first we need to sort by the 2nd invariant: alphabetically
+
+    wordfreq_keys = sorted(wordfreq)
+
+    # first we need to convert the dictionary into a list of tuples
+
+    for key in wordfreq_keys:
+        heapq.heappush(wordpriority, (wordfreq[key], key))
+
+
+    return [heapq.heappop(wordpriority) for i in range (len(wordpriority))]
+
+
