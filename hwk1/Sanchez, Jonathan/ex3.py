13260fbbbcf4f5bf4f8d66f9048c94fe5bc40b52
Jonathan Sanchez <jesanchez@wpi.edu>
2018-10-28 21:23:14 -0400

    removed main for testing code. ex3 complete

--- hwk1/Sanchez, Jonathan/ex3.py
+++ hwk1/Sanchez, Jonathan/ex3.py
@@ -49,8 +49,8 @@ def wordfreq_to_wordpriority(wordfreq):
     return [heapq.heappop(wordpriority) for i in range (len(wordpriority))]
 
 
-if __name__ == "__main__":
-    wordlist = file_to_wordlist("/home/jesanchez/git/rbe3002hwk/hwk1/alice_ascii.txt")
-    wordfreq = wordlist_to_wordfreq(wordlist)
-    wordpriority = wordfreq_to_wordpriority(wordfreq)
-    print wordpriority
+# if __name__ == "__main__":
+#     wordlist = file_to_wordlist("/home/jesanchez/git/rbe3002hwk/hwk1/alice_ascii.txt")
+#     wordfreq = wordlist_to_wordfreq(wordlist)
+#     wordpriority = wordfreq_to_wordpriority(wordfreq)
+#     print wordpriority
